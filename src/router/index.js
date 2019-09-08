52cb390d59c439642a5106f3e1bb4040d9825e7a
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-08 15:32:53 -0700

    Route params access

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import VideoWatch from '../views/VideoWatch.vue';
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/video/:id",
+    name: "video-watch",
+    component: VideoWatch,
+    props: true
   }
 ];
 
---
+++ src/views/VideoWatch.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>HEY {{this.$route.params.id}}</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>

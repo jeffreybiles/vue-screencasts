4229afc784c80f4fe1ea309b49330f833c4f84c7
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-08 17:42:36 -0700

    VideoWatch page

---
+++ src/views/VideoWatch.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img :src="video.thumbnail" alt="">
+    <h1>{{video.name}}</h1>
+    <div v-html="video.description"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    video(){
+      return this.$store.state.videos.find(vid => vid.id == this.$route.params.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    max-width: 50%;
+  }
+</style>

a8c00a43ef316ce3d7cf41c09f72cdfedf6b9544
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-08 17:59:29 -0700

    Play video in video player

--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,7 @@ export default new Vuex.Store({
         name: "ES2015 Arrow Functions",
         description: `<p>ES2015 (aka ES6) has some great ways to make your code easier to write and understand. In this episode, we cover two different ways that you can make your code clearer by removing the 'function' keyword.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Arrow+Function.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/38-+es2015-+functions+minus+'function'.mp4",
       },
       {
         id: 2,
@@ -18,6 +19,7 @@ export default new Vuex.Store({
         description: `<p>Template strings are an incredibly useful new feature in ES2015... and you can use them in your Ember apps today!</p>
                       <p>Here are 3 cool things that template strings enable.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Template+Strings.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/42-+ES2015+template+strings.mp4",
       },
       {
         id: 3,
@@ -25,6 +27,7 @@ export default new Vuex.Store({
         description: `<p>Before modules, javascript code loading was a mess. Now it's pretty amazing.</p>
                       <p>Learn about ES2015 modules and how they work together with ember-cli.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+ES2015+Modules.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/EmberScreencast+62+-+ES2015+Modules+-+Import%2C+Export.mp4",
       },
       {
         id: 4,
@@ -32,6 +35,7 @@ export default new Vuex.Store({
         description: `<p>Promises are a tool for handling asynchronous communication, and they are commonly used in Ember applications. They’re even built in to Ember Data, route handling, and other parts of the Ember source.</p>
                       <p>This episode introduces the then, catch, and finally blocks, how to chain them together, and how to use them with either named or anonymous functions.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Promise+Basics.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/EmberScreencast+92+-+Promises+Basics.mp4",
       },
       {
         id: 5,
@@ -39,6 +43,7 @@ export default new Vuex.Store({
         description: `<p>Sometimes you want to start a new promise chain or have greater control over the timing of events. By manually creating a promise, you can do that.</p>
                       <p>In this video we cover creating a new promise and resolving or rejecting the promise. We use two examples- a custom AJAX login and an Ember Data findAll request.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Creating+New+Promises.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/EmberScreencast+93+-+Creating+New+Promises.mp4",
       },
       {
         id: 6,
@@ -46,6 +51,7 @@ export default new Vuex.Store({
         description: `<p>Sometimes promises have to happen in an exact order, but often there will be several calls that can be made concurrently. RSVP’s Promise implementation allows that.</p>
                       <p>In this video we show how to use the hash method, one of several methods available to do concurrent promises.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Multiple+Concurrent+Promises.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/EmberScreencast+95+-+Multiple+Concurrent+Promises.mp4",
       },
       {
         id: 7,
@@ -53,6 +59,7 @@ export default new Vuex.Store({
         description: `<p>Destructuring is really cool, can save you code, and you’re probably already using a small portion of its power.</p>
                       <p>Learn more about this cool new ES2015/ES6 feature, including how to destructure deeply nested objects, how to use it with Ember’s import statement, and how to use it with arrays.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Destructuring.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/EmberScreencast+114+-+ES2015+Destructuring.mp4",
       },
       {
         id: 8,
@@ -60,6 +67,7 @@ export default new Vuex.Store({
         description: `<p>Decorators let you easily and repeatedly add functionality to existing functions.</p>
                       <p>In this episode we go over how to use and create decorators, creating two useful decorators that demonstrate different parts of the API.</p>`,
         thumbnail: 'https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Decorators.png',
+        videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/EmberScreencast+115+-+ES2016+Decorators.mp4",
       }
     ],
   },

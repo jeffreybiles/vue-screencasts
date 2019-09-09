792382366cbc287cd2f989aa9a58457c5bd96460
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-09 08:07:44 -0700

    Play videos

--- src/views/VideoWatch.vue
+++ src/views/VideoWatch.vue
@@ -1,23 +1,44 @@
 <template>
   <div>
-    <img :src="video.thumbnail" alt="">
+    <video-player class="video-player-box"
+                  ref="videoPlayer"
+                  :options="playerOptions">
+    </video-player>
     <h1>{{video.name}}</h1>
     <div v-html="video.description"></div>
   </div>
 </template>
 
 <script>
+import 'video.js/dist/video-js.css'
+ 
+import { videoPlayer } from 'vue-video-player'
 export default {
+  components: {
+    videoPlayer
+  },
   computed: {
     video(){
       return this.$store.state.videos.find(vid => vid.id == this.$route.params.id)
+    },
+    playerOptions(){
+      return {
+        language: 'en',
+        playbackRates: [0.7, 1.0, 1.5, 2.0, 2.5, 3.0],
+        sources: [{
+          type: "video/mp4",
+          src: this.video.videoUrl
+        }],
+        poster: this.video.thumbnail,
+      }
     }
   }
 }
 </script>
 
-<style scoped>
-  img {
-    max-width: 50%;
+<style>
+  .video-player-box .video-js {
+    margin: auto;
   }
+
 </style>

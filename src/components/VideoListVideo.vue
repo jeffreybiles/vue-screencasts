c02af9548457789277c4bdfbd53c5fa314c9cf9d
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-09 14:03:46 -0700

    VideoListVideo component, in preparation for tags page

---
+++ src/components/VideoListVideo.vue
@@ -0,0 +1,46 @@
+<template>
+  <router-link :to="{ name: 'video-watch', params: { id: video.id }}">
+    <div class="video-box">
+      <img :src="video.thumbnail" />
+      <div>
+        <h3>{{ video.name }}</h3>
+        <div v-html="video.description"></div>
+        <button v-for="tag_id in video.tag_ids" :key="tag_id">
+          {{ getTag(tag_id).name }}
+        </button>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+export default {
+  name: 'video-list-video',
+  computed: {
+    ...mapState(['tags']),
+    ...mapGetters(['getTag'])
+  },
+  props: ['video']
+
+}
+</script>
+
+<style scoped lang="scss">
+  .video-box {
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+
+    text-align: left;
+
+    display: flex;
+    justify-content: flex-start;
+
+    img {
+      width: 200px;
+      padding: 10px;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,52 +3,27 @@
     <h1>Videos</h1>
     <div class="video-container">
       <div v-for="video in videos" :key="video.name">
-        <router-link :to="{ name: 'video-watch', params: { id: video.id }}">
-          <div class="video-box">
-            <img :src="video.thumbnail" />
-            <div>
-              <h3>{{ video.name }}</h3>
-              <div v-html="video.description"></div>
-              <button v-for="tag_id in video.tag_ids" :key="tag_id">
-                {{ getTag(tag_id).name }}
-              </button>
-            </div>
-          </div>
-        </router-link>
+        <VideoListVideo :video="video"></VideoListVideo>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex';
+import { mapState } from 'vuex';
+import VideoListVideo from '@/components/VideoListVideo';
 export default {
   name: "home",
-  components: {},
+  components: {
+    VideoListVideo
+  },
   computed: {
-    ...mapState(['videos', 'tags']),
-    ...mapGetters(['getTag'])
+    ...mapState(['videos'])
   }
 };
 </script>
 
 <style scoped lang="scss">
   .video-container {
-    .video-box {
-      border: 1px solid black;
-      border-radius: 10px;
-      margin: 10px;
-      padding: 10px;
-
-      text-align: left;
-
-      display: flex;
-      justify-content: flex-start;
-
-      img {
-        width: 200px;
-        padding: 10px;
-      }
-    }
   }
 </style>

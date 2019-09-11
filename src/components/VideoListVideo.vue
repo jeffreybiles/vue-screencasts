ec76a6d337d0bbfc334cd94491daf757a63be056
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-11 13:03:55 -0700

    rearrange it again

--- src/components/VideoListVideo.vue
+++ src/components/VideoListVideo.vue
@@ -1,19 +1,17 @@
 <template>
-  <v-card width="340px" hover :to="{ name: 'video-watch', params: {id: video.id}}" ripple=false>
-    <v-card-text>
-      <v-img :src="video.thumbnail" />
-      <div class="title">{{ video.name }}</div>
-    </v-card-text>
-    <v-card-actions class="justify-center">
-        <span v-for="tag_id in video.tag_ids" :key="`${video.id}-${tag_id}`">
-          <v-btn rounded 
-                 color="green lighten-2"
-                 class="mr-2"
-                 @mousedown.stop
-                 :to="{ name: 'tag', params: {id: tag_id}}">
-            {{ getTag(tag_id).name }}
-          </v-btn>
-        </span>
+  <v-card width="340px" hover :to="{ name: 'video-watch', params: {id: video.id}}">
+    <v-img :src="video.thumbnail" />
+    <v-card-title>{{ video.name }}</v-card-title>
+
+    <v-card-actions>
+      <span v-for="tag_id in video.tag_ids" :key="`${video.id}-${tag_id}`">
+        <v-chip label 
+                color="green lighten-2"
+                class="mr-2"
+                :to="{ name: 'tag', params: {id: tag_id}}">
+          {{ getTag(tag_id).name }}
+        </v-chip>
+      </span>
     </v-card-actions>
   </v-card>
 </template>
@@ -29,11 +27,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  img {
-    max-width: 100%;
-    padding: 10px;
-  }
-  .v-card {
-    width: 340px;
+  .tags {
+    bottom: 10px;
+    left: 5px;
+    position: absolute;
   }
 </style>

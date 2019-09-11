7dc25a58a1142d0d3489dd7ca35612d8be9f3c21
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-11 10:31:00 -0700

    Install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vue Screencasts</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+        <span class="mr-2">Login</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
+  name: 'App',
   mounted(){
     this.$store.dispatch('loadVideos');
   },
-}
+  components: {
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
-button.tag-button {
-  background-color: #72C9A2;
-  border-radius: 5px;
-  padding: 5px;
-  margin: 3px;
-  font-size: 16px;
-  cursor: pointer;
-}
-</style>

3be10e09fb0f65d59086f7a57d1e9a691c333301
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-14 16:18:57 -0700

    Show video card preview

--- src/views/VideoCreate.vue
+++ src/views/VideoCreate.vue
@@ -1,26 +1,37 @@
 <template>
   <v-container>
-    <div class="field">
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model="video.name" />
-    </div>
-    <div class="field">
-      <label for="description">Description</label>
-      <textarea id="description" v-model="video.description" />
-    </div>
-    <div class="field">
-      <label for="thumbnail">Thumbnail URL</label>
-      <input type="text" id="thumbnail" v-model="video.thumbnail">
-    </div>
-    <div class="field">
-      <label for="videoUrl">Video URL</label>
-      <input type="text" id="videoUrl" v-model="video.videoUrl">
-    </div>
+    <v-row>
+      <v-col sm="5" md="3" cols="12">
+        <div class="field">
+          <label for="name">Name</label>
+          <input type="text" id="name" v-model="video.name" />
+        </div>
+        <div class="field">
+          <label for="description">Description</label>
+          <textarea id="description" v-model="video.description" />
+        </div>
+        <div class="field">
+          <label for="thumbnail">Thumbnail URL</label>
+          <input type="text" id="thumbnail" v-model="video.thumbnail">
+        </div>
+        <div class="field">
+          <label for="videoUrl">Video URL</label>
+          <input type="text" id="videoUrl" v-model="video.videoUrl">
+        </div>
+      </v-col>
+      <v-col sm="7" md="9" cols="12">
+        <VideoListVideo :video="video" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+  import VideoListVideo from '@/components/VideoListVideo.vue';
   export default {
+    components: {
+      VideoListVideo
+    },
     data() {
       return {
         video: {}

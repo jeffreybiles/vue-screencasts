247163aac5c1b5659b3b136cb352d3a47776e120
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-14 16:13:16 -0700

    Video Input

--- src/views/VideoCreate.vue
+++ src/views/VideoCreate.vue
@@ -1,25 +1,45 @@
 <template>
-  <div>
-    <div>
-      <input type="text" v-model="firstInput" />
+  <v-container>
+    <div class="field">
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="video.name" />
     </div>
-    <div>You typed: {{ firstInput }}</div>
-  </div>
+    <div class="field">
+      <label for="description">Description</label>
+      <textarea id="description" v-model="video.description" />
+    </div>
+    <div class="field">
+      <label for="thumbnail">Thumbnail URL</label>
+      <input type="text" id="thumbnail" v-model="video.thumbnail">
+    </div>
+    <div class="field">
+      <label for="videoUrl">Video URL</label>
+      <input type="text" id="videoUrl" v-model="video.videoUrl">
+    </div>
+  </v-container>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        firstInput: ''
+        video: {}
       }
     },
   }
 </script>
 
 <style lang="scss" scoped>
-  input {
-    background-color: white;
-    border: 1px solid black;
+
+  .field {
+    label {
+      display: block;
+      margin-bottom: 2px;
+    }
+    input, textarea {
+      background-color: white;
+      border: 1px solid black;
+    }
+    padding-bottom: 10px;
   }
 </style>

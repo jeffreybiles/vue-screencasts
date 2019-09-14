96e439e1ae402de1c9612aa1636bd846b1e8fb12
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-14 16:02:20 -0700

    VideoCreate route + first input

---
+++ src/views/VideoCreate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <input type="text" v-model="firstInput" />
+    </div>
+    <div>You typed: {{ firstInput }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        firstInput: ''
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  input {
+    background-color: white;
+    border: 1px solid black;
+  }
+</style>

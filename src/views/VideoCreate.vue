30209bef220e5e551292f3ee08c2c42565ce1640
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-14 22:50:47 -0700

    Use vuetify components

--- src/views/VideoCreate.vue
+++ src/views/VideoCreate.vue
@@ -2,25 +2,11 @@
   <v-container>
     <v-row>
       <v-col sm="5" md="3" cols="12">
-        <div class="field">
-          <label for="name">Name</label>
-          <input type="text" id="name" v-model="video.name" />
-        </div>
-        <div class="field">
-          <label for="description">Description</label>
-          <textarea id="description" v-model="video.description" />
-        </div>
-        <div class="field">
-          <label for="thumbnail">Thumbnail URL</label>
-          <input type="text" id="thumbnail" v-model="video.thumbnail">
-        </div>
-        <div class="field">
-          <label for="videoUrl">Video URL</label>
-          <input type="text" id="videoUrl" v-model="video.videoUrl">
-        </div>
-        <div class="field">
-          <v-btn @click="createVideo">Create Video</v-btn>
-        </div>
+        <v-text-field label="Name" v-model="video.name" />
+        <v-text-field label="Description" v-model="video.description" />
+        <v-text-field label="Thumbnail URL" v-model="video.thumbnail" />
+        <v-text-field label="Video URL" v-model="video.videoUrl" />
+        <v-btn @click="createVideo">Create Video</v-btn>
       </v-col>
       <v-col sm="7" md="9" cols="12">
         <VideoListVideo :video="video" />
@@ -50,16 +36,4 @@
 </script>
 
 <style lang="scss" scoped>
-
-  .field {
-    label {
-      display: block;
-      margin-bottom: 2px;
-    }
-    input, textarea {
-      background-color: white;
-      border: 1px solid black;
-    }
-    padding-bottom: 10px;
-  }
 </style>

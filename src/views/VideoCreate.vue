9cf3d6090cf943fa478a846fbd194248e9292495
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-16 11:32:35 -0700

    Use Vuetify input components

--- src/views/VideoCreate.vue
+++ src/views/VideoCreate.vue
@@ -3,22 +3,13 @@
     <v-row>
       <v-col md="3" cols="12">
         <h1>Video Create Page</h1>
-        <div class="field">
-          <label for="name">Name</label>
-          <input type="text" id="name" v-model="video.name" />
-        </div>
-        <div class="field">
-          <label for="description">Description</label>
-          <input type="text" id="description" v-model="video.description" />
-        </div>
-        <div class="field">
-          <label for="thumbnail">Thumbnail URL</label>
-          <input type="text" id="thumbnail" v-model="video.thumbnail" />
-        </div>
-        <div class="field">
-          <label for="videoUrl">Video URL</label>
-          <input type="text" id="videoUrl" v-model="video.videoUrl" />
-        </div>
+        <v-text-field v-model="video.name" label="Name" />
+        <v-textarea v-model="video.description" label="Description" />
+        <v-text-field v-model="video.thumbnail" label="Thumbnail URL" />
+        <v-text-field v-model="video.videoUrl" 
+                      label="Video URL" 
+                      hint="If you want our friends in China to be able to watch this, please use Amazon S3 or similar instead of Youtube and Vimeo." />
+
         <v-btn @click="createVideo">Create Video</v-btn>
       </v-col>
       <v-col md="9" cols="12">
@@ -48,15 +39,5 @@ import VideoListVideo from '@/components/VideoListVideo';
   }
 </script>
 
-<style lang="scss" scoped>
-.field {
-  input {
-    border: 1px solid black;
-  }
-  label {
-    display: block;
-  }
-  padding-bottom: 10px;
-}
-  
+<style lang="scss" scoped>  
 </style>

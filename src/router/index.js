12431a7d88293dd33a8d1b465b394bc75b8d3493
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-18 19:05:47 -0700

    Admin page + Edit page + Delete button

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Home from "../views/Home.vue";
 import VideoWatch from "../views/VideoWatch.vue";
 import VideoCreate from "../views/VideoCreate.vue";
 import TagVideoList from "../views/TagVideoList.vue";
+import AdminVideoList from "../views/AdminVideoList.vue";
+import AdminVideoEdit from "../views/AdminVideoEdit.vue";
+
 
 Vue.use(VueRouter);
 
@@ -21,6 +24,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/admin/videos",
+    name: "admin-video-list",
+    component: AdminVideoList,
+  },
+  {
+    path: "/admin/videos/:id/edit",
+    name: "admin-video-edit",
+    component: AdminVideoEdit,
+    params: true
   },
   {
     path: "/video/new",
--- src/store/index.js
+++ src/store/index.js
@@ -28,6 +28,17 @@ export default new Vuex.Store({
     ADD_VIDEO(state, video) {
       let videos = state.videos.concat(video);
       state.videos = videos;
+    },
+    DELETE_VIDEO(state, videoId){
+      let videos = state.videos.filter(v => v.id != videoId)
+      state.videos = videos;
+    },
+    EDIT_VIDEO(state, video) {
+      state.videos.forEach(v => {
+        if(v.id == video.id) {
+          v = video;
+        }
+      })
     }
   },
   actions: {
@@ -57,6 +68,18 @@ export default new Vuex.Store({
       let savedVideo = response.data.data.attributes;
       commit('ADD_VIDEO', savedVideo);
       return savedVideo;
+    },
+    async deleteVideo({commit}, video) {
+      let response = await Api().delete(`/videos/${video.id}`);
+      if(response.status == 200 || response.status == 204){
+        commit('DELETE_VIDEO', video.id);
+      }
+    },
+    async editVideo({commit}, video) {
+      let response = await Api().put(`/videos/${video.id}`, video);
+      let newVideo = response.data.data.attributes;
+      commit('EDIT_VIDEO', newVideo);
+      return newVideo;
     }
   },
   modules: {},

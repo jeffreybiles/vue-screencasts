f46a635f54e091731a47b1ae7f123730a033247b
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-19 23:10:59 -0700

    v-form

--- src/views/AdminVideoEdit.vue
+++ src/views/AdminVideoEdit.vue
@@ -1,22 +1,24 @@
 <template>
   <v-container>
-    <v-text-field v-model="video.name"
-                  label="Name" 
-                  counter=50 
-                  :rules="[required('name'), minLength('name', 5), maxLength('name', 50)]" />
-    <v-textarea v-model="video.description" 
-                label="Description"
-                counter=true
-                :rules="[required('description'), minLength('description', 20)]"/>
-    <v-text-field v-model="video.thumbnail" 
-                  label="Thumbnail URL" 
-                  :rules="[required('thumbnail URL')]"/>
-    <v-text-field v-model="video.videoUrl" 
-                  label="Video URL" 
-                  :rules="[required('video URL')]"
-                  hint="If you want our friends in China to be able to watch this, please use Amazon S3 or similar instead of Youtube and Vimeo." />
+    <v-form v-model="valid">
+      <v-text-field v-model="video.name"
+                    label="Name" 
+                    counter=50 
+                    :rules="[required('name'), minLength('name', 5), maxLength('name', 50)]" />
+      <v-textarea v-model="video.description" 
+                  label="Description"
+                  counter=true
+                  :rules="[required('description'), minLength('description', 20)]"/>
+      <v-text-field v-model="video.thumbnail" 
+                    label="Thumbnail URL" 
+                    :rules="[required('thumbnail URL')]"/>
+      <v-text-field v-model="video.videoUrl" 
+                    label="Video URL" 
+                    :rules="[required('video URL')]"
+                    hint="If you want our friends in China to be able to watch this, please use Amazon S3 or similar instead of Youtube and Vimeo." />
 
-    <v-btn @click="saveVideo">Save Video</v-btn>
+      <v-btn @click="saveVideo" :disabled="!valid">Save Video</v-btn>
+    </v-form>
   </v-container>
 </template>
 

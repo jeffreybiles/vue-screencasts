7f47778e47626c3188d96bec4f9645f5adbf416f
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-20 14:00:10 -0700

    Abstract to VideoEditForm

--- src/views/AdminVideoEdit.vue
+++ src/views/AdminVideoEdit.vue
@@ -1,44 +1,16 @@
 <template>
   <v-container>
-    <v-form v-model="valid">
-      <v-text-field v-model="video.name" 
-                    label="Name" 
-                    counter=50
-                    :rules="[required('name'), minLength('name', 5), maxLength('name', 50)]" />
-      <v-textarea v-model="video.description" 
-                  label="Description" 
-                  counter=true
-                  :rules="[required('description'), minLength('description', 20)]"/>
-      <v-text-field v-model="video.thumbnail" 
-                    label="Thumbnail URL" 
-                    :rules="[required('thumbnail URL')]" />
-      <v-text-field v-model="video.videoUrl" 
-                    label="Video URL" 
-                    :rules="[required('video URL')]"
-                    hint="If you want our friends in China to be able to watch this, please use Amazon S3 or similar instead of Youtube and Vimeo." />
-
-      <v-btn @click="saveVideo" :disabled="!valid">Save Video</v-btn>
-    </v-form>
+    <VideoEditForm :video="video" :saveVideo="saveVideo" buttonText="Save Video" />
   </v-container>
 </template>
 
 <script>
   import { mapState } from 'vuex';
+  import VideoEditForm from '@/components/VideoEditForm.vue';
 
   export default {
-    data() {
-      return {
-        valid: false,
-        required(propertyType) { 
-          return v => v && v.length > 0 || `You must input a ${propertyType}`
-        },
-        minLength(propertyType, minLength) {
-          return v => v && v.length >= minLength || `${propertyType} must be at least ${minLength} characters`
-        },
-        maxLength(propertyType, maxLength) {
-          return v => v && v.length <= maxLength || `${propertyType} must be less than ${maxLength} characters`
-        }
-      }
+    components: {
+      VideoEditForm,
     },
     computed: {
       ...mapState(['videos']),

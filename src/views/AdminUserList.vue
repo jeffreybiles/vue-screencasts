b448cb601c0b28e75344ba8157a00df8be541677
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-23 10:17:18 -0700

    Login/logout from admin/users page + localstorage

---
+++ src/views/AdminUserList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="user in users" :key="user.id">
+      {{user.name}} <v-btn x-small @click="loginUser(user)">Login</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapState(['users'])
+    },
+    mounted(){
+      this.$store.dispatch("loadUsers")
+    },
+    methods: {
+      loginUser(user){
+        this.$store.dispatch('loginUser', user)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

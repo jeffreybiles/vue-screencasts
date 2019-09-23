4ff1c28bf95beed9930c8a0757551a88555b0407
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-23 16:57:55 -0700

    User Registration

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -1,41 +1,19 @@
 <template>
   <v-container>
-    <v-form v-model="valid">
-      <v-text-field v-model="loginInfo.email" 
-                    label="Email" 
-                    :rules="[required('email'), emailFormat()]"/>
-
-      <v-text-field v-model="loginInfo.password"
-                    label="Password"
-                    :type="showPassword ? 'text' : 'password'" 
-                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                    @click:append="showPassword = !showPassword"
-                    counter=true
-                    :rules="[required('password'), minLength('password', 8)]"
-                    />
-
-      <v-btn @click="loginUser" :disabled="!valid">Login</v-btn>
-    </v-form>
+    <UserAuthForm :submitForm="loginUser" buttonText="Login" />
   </v-container>
 </template>
 
 <script>
-  import validations from "@/utils/validations";
+  import UserAuthForm from "@/components/UserAuthForm";
 
   export default {
-    data() {
-      return {
-        showPassword: true,
-        loginInfo: {
-          email: '',
-          password: ''
-        },
-        ...validations
-      }
+    components: {
+      UserAuthForm
     },
     methods: {
-      async loginUser() {
-        let user = await this.$store.dispatch('loginUser', this.loginInfo);
+      async loginUser(loginInfo) {
+        let user = await this.$store.dispatch('loginUser', loginInfo);
         if(user.error){
           alert(user.error)
         } else {

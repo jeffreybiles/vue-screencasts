8e96f9caafbaa9e3c1482b87ad9762d4b5d20330
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-23 16:57:55 -0700

    User Registration

--- src/store/index.js
+++ src/store/index.js
@@ -116,6 +116,17 @@ export default new Vuex.Store({
         return {error: "Email/password combination was incorrect.  Please try again."}
       }
       
+    },
+    async registerUser({commit}, registrationInfo) {
+      try {
+        let response = await Api().post('/users', registrationInfo);
+        let user = response.data.data.attributes;
+  
+        commit('SET_CURRENT_USER', user);
+        return user;
+      } catch {
+        return {error: "There was an error.  Please try again."}
+      }
     }
   },
   modules: {},

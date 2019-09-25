5d8abe71e37831adb0269a4ead693d408ef38633
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-25 16:23:35 -0700

    Add authentication token to API calls

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 export default () => {
+  let currentUser = JSON.parse(window.localStorage.currentUser);
   return axios.create({
     baseURL: 'http://localhost:3000/api',
     withCredentials: false,
     headers: {
       Accept: "application/json",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      Authorization: currentUser && currentUser.token
     }
   });
 }

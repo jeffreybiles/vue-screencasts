4856d0d081d106eace67e3a10edc008d5d94f5a8
jeffreybiles <bilesjeffrey@gmail.com>
2019-09-28 13:04:18 -0700

    First implementation of one-page snackbar

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -2,6 +2,13 @@
   <v-container>
     <h1>Login</h1>
     <UserAuthForm :submitForm="loginUser" buttonText="Login" />
+
+    <v-snackbar v-model="snackbar" :color="snackbarColor" :timeout="0">
+      {{ snackbarText }}
+      <v-btn text @click="snackbar = false">
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -12,13 +19,24 @@
     components: {
       UserAuthForm
     },
+    data() {
+      return {
+        snackbar: true,
+        snackbarText: "TESTING",
+        snackbarColor: "",
+      }
+    },
     methods: {
       async loginUser(loginInfo) {
         let user = await this.$store.dispatch('loginUser', loginInfo);
         if(user.error){
-          alert(user.error)
+          this.snackbarText = user.error;
+          this.snackbar = true;
+          this.snackbarColor = 'error';
         } else {
-          alert('Thank you for signing in, ' + user.name);
+          this.snackbarText = 'Thank you for signing in, ' + user.name;
+          this.snackbar = true;
+          this.snackbarColor = 'success'
         }
       }
     },

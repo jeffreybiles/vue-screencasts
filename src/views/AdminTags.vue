afee8b530ec651fbfae46aef2d73b182f86dc0f6
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-04 10:24:43 -0700

    Start of tags page

---
+++ src/views/AdminTags.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="flex-table">
+      <div>Name</div>
+      <div># Videos</div>
+      <div>Actions</div>
+    </div>
+    <div v-for="tag in tags" :key="tag.id" class="flex-table">
+      <div>{{ tag.name }}</div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+  export default {
+    computed: {
+      ...mapState(['tags'])
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .flex-table {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 33%);
+    padding: 10px;
+    border-bottom: 1px black solid;
+
+    &:nth-of-type(2n) {
+      background-color: #dedede;
+    }
+
+    .actions {
+      * {
+        padding-right: 15px
+      }
+    }
+  }
+</style>

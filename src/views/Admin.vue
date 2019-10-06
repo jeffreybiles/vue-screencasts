c812acc8c3fa2134f001b1225b79594fee3946aa
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-05 17:02:48 -0700

    Tags Page - Read

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,6 +2,7 @@
   <v-container>
     <v-tabs>
       <v-tab to="/admin/videos">Videos</v-tab>
+      <v-tab to="/admin/tags">Tags</v-tab>
       <v-tab to="/admin/users">Users</v-tab>
     </v-tabs>
     <router-view></router-view>

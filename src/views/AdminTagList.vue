652cce43788bf6028e95b9faf105b05af3ebedd0
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-05 17:02:48 -0700

    Tags Page - Read

---
+++ src/views/AdminTagList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="flex-table">
+      <div>Name</div>
+      <div># videos</div>
+      <div></div>
+    </div>
+    <div v-for="tag in tags" :key="tag.id" class="flex-table">
+      <div>{{tag.name}}</div>
+      <div>
+        <router-link :to="{ name: 'tag', params: { id: tag.id }}">
+          {{tag.video_ids.length}}
+        </router-link>
+      </div>
+      <div>
+        <v-btn x-small>Do Something</v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+  export default {
+    computed: {
+      ...mapState(['tags'])
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .flex-table {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 33%);
+    padding: 10px;
+    border-bottom: 1px black solid;
+
+    &:nth-of-type(2n) {
+      background-color: #dedede;
+    }
+
+    .actions {
+      * {
+        padding-right: 15px
+      }
+    }
+  }
+</style>

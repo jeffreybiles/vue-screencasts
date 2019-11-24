9a79c71b53ca8172c7c6fdeaffe9e03109586287
jeffreybiles <bilesjeffrey@gmail.com>
2019-11-24 08:33:31 -0800

    TagVideoList page

---
+++ src/views/TagVideoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Videos with Tag "{{ tag.name }}"</h1>
+    <div v-for="video in videosOnTag" :key="video">
+      <VideoListVideo :video="video"></VideoListVideo>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+import VideoListVideo from '@/components/VideoListVideo'
+
+export default {
+  components: {
+    VideoListVideo
+  },
+  computed: {
+    ...mapState(['videos']),
+    ...mapGetters(['getTag']),
+    tag(){
+      return this.getTag(this.$route.params.id)
+    },
+    videosOnTag(){
+      return this.videos.filter(v => this.tag.video_ids.includes(v.id.toString()))
+    }
+  }  
+}
+</script>

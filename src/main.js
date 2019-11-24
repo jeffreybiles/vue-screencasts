9b5744fdb08c75e977effb090c9bbd738827ae18
jeffreybiles <bilesjeffrey@gmail.com>
2019-11-24 08:04:15 -0800

    First JSONAPI relationship, videos and tags

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Server, JSONAPISerializer, Model } from "miragejs";
+import { Server, JSONAPISerializer, Model, hasMany } from "miragejs";
 import videoJSON from "./mirage/videos.json";
+import tagsJSON from "./mirage/tags.json";
 
 let server = new Server({
   serializers: {
-    application: JSONAPISerializer
+    application: JSONAPISerializer,
+    video: JSONAPISerializer.extend({
+      include: ['tags']
+    }),
+    tag: JSONAPISerializer.extend({
+      include: ['videos']
+    })
   },
   fixtures: {
-    videos: videoJSON
+    videos: videoJSON,
+    tags: tagsJSON
   },
   models: {
-    video: Model
+    video: Model.extend({
+      tags: hasMany()
+    }),
+    tag: Model.extend({
+      videos: hasMany()
+    })
   },
   routes() {
     this.get("/videos")
--- src/store/index.js
+++ src/store/index.js
@@ -6,22 +6,33 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    videos: []
+    videos: [],
+    tags: []
   },
   mutations: {
     SET_VIDEOS(state, videos) {
       state.videos = videos
+    },
+    SET_TAGS(state, tags) {
+      state.tags = tags
     }
   },
   actions: {
     async loadVideos({commit}){
       let response = await Api().get('/videos');
       let videos = response.data.data;
+      let tags = response.data.included.filter(i => i.type === "tags")
+      tags.forEach(t => {
+        t.attributes.id = t.id;
+        t.attributes.video_ids = t.relationships.videos.data.map(v => v.id);
+      });
       videos.forEach(v => {
         v.attributes.id = v.id;
+        v.attributes.tag_ids = v.relationships.tags.data.map(t => t.id);
       });
 
       commit('SET_VIDEOS', videos.map(v => v.attributes));
+      commit('SET_TAGS', tags.map(t => t.attributes));
     }
   },
   modules: {}
